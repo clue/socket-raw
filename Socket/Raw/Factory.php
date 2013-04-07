abe0335abd573bbd4b0596d403e26ce29b4fa4e7
Christian Lück <christian@lueck.tv>
2013-04-07 11:22:49 +0200

    Simple helpers to create client and server sockets

--- Socket/Raw/Factory.php
+++ Socket/Raw/Factory.php
@@ -3,9 +3,27 @@
 namespace Socket\Raw;
 
 use \Exception;
+use \InvalidArgumentException;
 
 class Factory
 {
+    public function createClient($address)
+    {
+        $socket = $this->createFromString($address);
+        return $socket->connect($address);
+    }
+
+    public function createServer($address)
+    {
+        $socket = $this->createFromString($address);
+        $socket->bind($address);
+
+        if ($socket->getType() === SOCK_STREAM) {
+            $socket->listen();
+        }
+        return $socket;
+    }
+
     public function createTcp4()
     {
         return $this->create(AF_INET, SOCK_STREAM, SOL_TCP);
@@ -48,5 +66,46 @@ class Factory
             throw new Exception('Unable to create socket');
         }
         return new Socket($sock);
+    }
+
+    /**
+     *
+     * @param string $address (passed by reference in order to remove scheme, if present)
+     * @return \Socket\Raw\Socket
+     * @throws InvalidArgumentException if given address is invalid
+     * @throws Exception in case creating socket failed
+     */
+    protected function createFromString(&$address)
+    {
+        $scheme = 'tcp';
+
+        $pos = strpos($address, '://');
+        if ($pos !== false) {
+            $scheme = substr($address, 0, $pos);
+            $address = substr($address, $pos + 3);
+        }
+
+        if (strpos($address, '[') !== false && ($scheme === 'tcp' || $scheme === 'udp')) {
+            $scheme .= '6';
+        }
+
+        if ($scheme === 'tcp') {
+            $socket = $this->createTcp4();
+        } elseif ($scheme === 'udp') {
+            $socket = $this->createUdp4();
+        } elseif ($scheme === 'tcp6') {
+            $socket = $this->createTcp6();
+        } elseif ($scheme === 'udp6') {
+            $socket = $this->createUdp6();
+        } elseif ($scheme === 'unix') {
+            $socket = $this->createUnix();
+        } elseif ($scheme === 'udg') {
+            $socket = $this->createUdg();
+        } elseif ($scheme === 'icmp') {
+            $socket = $this->createIcmp();
+        } else {
+            throw new InvalidArgumentException('Invalid address scheme given');
+        }
+        return $socket;
     }
 }
