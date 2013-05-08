5fcfc9db9339a65ceacea8ddd95abcb96744f80a
Christian Lück <christian@lueck.tv>
2013-05-08 15:59:24 +0200

    Unify creating Factory instance

--- tests/FactoryTest.php
+++ tests/FactoryTest.php
@@ -5,42 +5,26 @@ use Socket\Raw\Factory;
 (include_once __DIR__.'/../vendor/autoload.php') OR die(PHP_EOL.'ERROR: composer autoloader not found, run "composer install" or see README for instructions'.PHP_EOL);
 
 class FactoryTest extends PHPUnit_Framework_TestCase{
-//     public function setUp(){
-//         $loop = React\EventLoop\Factory::create();
 
-//         $dnsResolverFactory = new React\Dns\Resolver\Factory();
-//         $dns = $dnsResolverFactory->createCached('8.8.8.8', $loop);
+    /**
+     * @var Socket\Raw\Factory
+     * @type Factory
+     */
+    protected $factory;
 
-//         $factory = new Socks\Factory($loop, $dns);
-
-//         $this->client = $factory->createClient('127.0.0.1', 9050);
-//     }
-
-//     /**
-//      * @expectedException UnexpectedValueException
-//      * @dataProvider providerInvalidAuthVersion
-//      */
-//     public function testInvalidAuthVersion($version)
-//     {
-//         $this->client->setAuth('username', 'password');
-//         $this->client->setProtocolVersion($version);
-//     }
-
-//     public function providerInvalidAuthVersion()
-//     {
-//         return array(array('4'), array('4a'));
-//     }
+    public function setUp()
+    {
+        $this->factory = new Factory();
+    }
 
     public function testConstructorWorks()
     {
-        $factory = new Factory();
-        $this->assertInstanceOf('Socket\Raw\Factory', $factory);
+        $this->assertInstanceOf('Socket\Raw\Factory', $this->factory);
     }
 
     public function testCreateTcp4()
     {
-        $factory = new Factory();
-        $socket = $factory->createTcp4();
+        $socket = $this->factory->createTcp4();
 
         $this->assertInstanceOf('Socket\Raw\Socket', $socket);
     }
@@ -49,16 +33,14 @@ class FactoryTest extends PHPUnit_Framework_TestCase{
     {
         // skip if no IPv6
 
-        $factory = new Factory();
-        $socket = $factory->createTcp6();
+        $socket = $this->factory->createTcp6();
 
         $this->assertInstanceOf('Socket\Raw\Socket', $socket);
     }
 
     public function testCreateUdp4()
     {
-        $factory = new Factory();
-        $socket = $factory->createUdp4();
+        $socket = $this->factory->createUdp4();
 
         $this->assertInstanceOf('Socket\Raw\Socket', $socket);
     }
@@ -67,8 +49,7 @@ class FactoryTest extends PHPUnit_Framework_TestCase{
     {
         // skip if no IPv6
 
-        $factory = new Factory();
-        $socket = $factory->createUdp6();
+        $socket = $this->factory->createUdp6();
 
         $this->assertInstanceOf('Socket\Raw\Socket', $socket);
     }
@@ -77,8 +58,7 @@ class FactoryTest extends PHPUnit_Framework_TestCase{
     {
         // skip if not unix
 
-        $factory = new Factory();
-        $socket = $factory->createUnix();
+        $socket = $this->factory->createUnix();
 
         $this->assertInstanceOf('Socket\Raw\Socket', $socket);
     }
@@ -87,17 +67,15 @@ class FactoryTest extends PHPUnit_Framework_TestCase{
     {
         // skip if not unix
 
-        $factory = new Factory();
-        $socket = $factory->createUdg();
+        $socket = $this->factory->createUdg();
 
         $this->assertInstanceOf('Socket\Raw\Socket', $socket);
     }
 
     public function testCreateIcmp4()
     {
-        $factory = new Factory();
         try {
-            $socket = $factory->createIcmp4();
+            $socket = $this->factory->createIcmp4();
         }
         catch (Exception $e) {
             if ($e->getCode() === SOCKET_EPERM) {
@@ -114,9 +92,8 @@ class FactoryTest extends PHPUnit_Framework_TestCase{
     {
         // skip if no IPv6
 
-        $factory = new Factory();
         try {
-            $socket = $factory->createIcmp6();
+            $socket = $this->factory->createIcmp6();
         }
         catch (Exception $e) {
             if ($e->getCode() === SOCKET_EPERM) {
@@ -134,8 +111,7 @@ class FactoryTest extends PHPUnit_Framework_TestCase{
      */
     public function testCreate($domain, $type, $protocol)
     {
-        $factory = new Factory();
-        $socket = $factory->create($domain, $type, $protocol);
+        $socket = $this->factory->create($domain, $type, $protocol);
 
         $this->assertInstanceOf('Socket\Raw\Socket', $socket);
     }
@@ -153,8 +129,7 @@ class FactoryTest extends PHPUnit_Framework_TestCase{
     {
         // skip if not unix
 
-        $factory = new Factory();
-        $sockets = $factory->createPair(AF_UNIX, SOCK_STREAM, 0);
+        $sockets = $this->factory->createPair(AF_UNIX, SOCK_STREAM, 0);
 
         $this->assertCount(2, $sockets);
         $this->assertInstanceOf('Socket\Raw\Socket', $sockets[0]);
@@ -163,20 +138,16 @@ class FactoryTest extends PHPUnit_Framework_TestCase{
 
     public function testCreateListenRandom()
     {
-        $factory = new Factory();
-
         // listen on a random free port
-        $socket = $factory->createListen(0);
+        $socket = $this->factory->createListen(0);
 
         $this->assertInstanceOf('Socket\Raw\Socket', $socket);
     }
 
     public function testCreateFromStringTcp4()
     {
-        $factory = new Factory();
-
         $address = 'tcp://127.0.0.1:80';
-        $socket = $factory->createFromString($address, $scheme);
+        $socket = $this->factory->createFromString($address, $scheme);
 
         $this->assertInstanceOf('Socket\Raw\Socket', $socket);
         $this->assertEquals('127.0.0.1:80', $address);
@@ -188,10 +159,8 @@ class FactoryTest extends PHPUnit_Framework_TestCase{
      */
     public function testCreateFromStringSchemelessTcp4()
     {
-        $factory = new Factory();
-
         $address = '127.0.0.1:80';
-        $socket = $factory->createFromString($address, $scheme);
+        $socket = $this->factory->createFromString($address, $scheme);
 
         $this->assertInstanceOf('Socket\Raw\Socket', $socket);
         $this->assertEquals('127.0.0.1:80', $address);
@@ -203,10 +172,8 @@ class FactoryTest extends PHPUnit_Framework_TestCase{
      */
     public function testCreateFromStringTcp6()
     {
-        $factory = new Factory();
-
         $address = 'tcp://[::1]:80';
-        $socket = $factory->createFromString($address, $scheme);
+        $socket = $this->factory->createFromString($address, $scheme);
 
         $this->assertInstanceOf('Socket\Raw\Socket', $socket);
         $this->assertEquals('[::1]:80', $address);
@@ -218,10 +185,8 @@ class FactoryTest extends PHPUnit_Framework_TestCase{
      */
     public function testCreateFromStringSchemelessTcp6()
     {
-        $factory = new Factory();
-
         $address = '[::1]:80';
-        $socket = $factory->createFromString($address, $scheme);
+        $socket = $this->factory->createFromString($address, $scheme);
 
         $this->assertInstanceOf('Socket\Raw\Socket', $socket);
         $this->assertEquals('[::1]:80', $address);
